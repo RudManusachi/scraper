bd4c49787050da4eefc3f5a5953e41dc578a503b
Rudolf Manusadzhian <rud.manusachi@gmail.com>
2019-06-24 02:48:38 -0700

    Make it work - setup bypass for tests - adapt tests for bypass Use HTTPoison to make requests Use Floki to extract links from html tests Happy path

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -14,10 +14,22 @@ defmodule Scraper do
   end
 
   defp request(url) do
-    ""
+    with %HTTPoison.Response{body: body} <- HTTPoison.get!(url) do
+      body
+    end
   end
 
   defp parse(html) do
-    %{assets: [], links: []}
+    assets =
+      html
+      |> Floki.find("img")
+      |> Floki.attribute("src")
+
+    links =
+      html
+      |> Floki.find("a")
+      |> Floki.attribute("href")
+
+    %{assets: assets, links: links}
   end
 end
--- test/scraper_test.exs
+++ test/scraper_test.exs
@@ -1,12 +1,63 @@
 defmodule ScraperTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Scraper
 
-  @url "https://theguardian.com"
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
 
-  test "fetch/1 returns %Map with lists :assets and :links" do
-    assert %{assets: assets, links: links} = Scraper.fetch(@url)
+  def use_bypass(bypass, body \\ nil) do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, body)
+    end)
+  end
+
+  test "fetch/1 returns %Map with lists :assets and :links", %{bypass: bypass} do
+    use_bypass(bypass, "")
+    assert %{assets: assets, links: links} = Scraper.fetch(endpoint_url(bypass.port))
     assert is_list(assets)
     assert is_list(links)
   end
+
+  test "happy path :assets and :links are the lists of urls present in the <img> and <a> tags respectively",
+       %{bypass: bypass} do
+    url = endpoint_url(bypass.port)
+
+    img_srcs = [
+      "#{url}/img1",
+      "#{url}/img2",
+      "#{url}/img3"
+    ]
+
+    a_hrefs = [
+      "#{url}/a1",
+      "#{url}/a2",
+      "#{url}/a3"
+    ]
+
+    body =
+      Enum.reduce(img_srcs, "", fn src, body ->
+        body <> "<img src=\"#{src}\" \/>"
+      end)
+
+    body =
+      Enum.reduce(a_hrefs, body, fn href, body ->
+        body <> "<a href=\"#{href}\">Link</a>"
+      end)
+
+    body = """
+    <div>
+      #{body}
+    </div>
+    """
+
+    use_bypass(bypass, body)
+
+    assert %{assets: assets, links: links} = Scraper.fetch(endpoint_url(bypass.port))
+    assert links == a_hrefs
+    assert assets == img_srcs
+  end
+
+  defp endpoint_url(port), do: "http://localhost:#{port}"
 end
